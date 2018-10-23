201874dd3dbb50bb26e0c6826e9e6168aed2279f
Hendro Saputra <hendro863@gmail.com>
2018-10-23 17:02:20 +0700

    modification color and add how to install laravel in readme.md

--- app/DetailTransaction.php
+++ app/DetailTransaction.php
@@ -2,9 +2,21 @@
 
 namespace App;
 
+use App\Account;
+use App\Models\Master\Transaction;
 use Illuminate\Database\Eloquent\Model;
 
 class DetailTransaction extends Model
 {
     protected $guarded = [];
+
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class);
+    }
 }
--- resources/views/components/parts/navs.blade.php
+++ resources/views/components/parts/navs.blade.php
@@ -1,11 +1,11 @@
 <h1 class="logo">
-	<a href="{{ route('~/') }}"><img src="images/harnods-logo.svg"/>Accounting</a>
+	<a href="{{ route('~/') }}"><img src="https://scontent-sin6-1.xx.fbcdn.net/v/t1.0-9/13077090_1595130447481656_7653648087860133963_n.png?_nc_cat=109&_nc_ht=scontent-sin6-1.xx&oh=27a67176edfd889b9030060b578eb7a8&oe=5C4B140F"/>Accounting</a>
 </h1>
 
 <div class="menu-toggle"></div>
 <ul class="menu">
 	<li class="active"><a href="{{ route('~/') }}">
-      <div class="fa fa-home"></div><span>Home</span></a>
+      <div class="fa fa-home" style="color:cornflowerblue;"></div><span style="color:cornflowerblue;">Home</span></a>
   	</li>
 	<li class=""><a href="{{ route('transaction') }}">
 	    <div class="fa fa-tasks"></div><span>Transaksi</span></a>
